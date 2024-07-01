2a76e7d85158fb9f11536c807d9cbe52b097361a
Bashir Amokomowo <barsheyr@gmail.com>
2024-07-01 10:42:59 +0100

    new push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,15 +27,18 @@ const Hero = () => {
               <div className="max-w-7xl  mx-auto lg:px-20 px-10 flex justify-center items-center">
                 <div className="flex flex-col lg:gap-x-[30px] items-center justify-center text-center lg:text-left lg:flex-row gap-y-8 lg:gap-y-0">
                   <div className="flex-1">
+                    <span className="text-blue-300 text-2xl"> Hi there! </span>
                     <h1
-                      className="text-4xl md:text-5xl lg:text-6xl font-bold mb-6"
+                      className="text-5xl md:text-4xl lg:text-7xl font-semibold mb-6"
                       data-aos="fade-down"
                       data-aos-delay="500"
                     >
-                      <span className="text-blue-300 text-sm"> Hi there! </span>
-                      I'm Mahmud "Demi" Akinbami <br /> Welcome to my creative
-                      space!
+                      I'm Mahmud Akinbami
                     </h1>
+
+                    <span className="text-blue-300 text-3xl">
+                      Welcome to my creative space!
+                    </span>
                   </div>
                   <div
                     className="flex-1"
